6c45281fdaf7361441c607770cae6894d65a40de
isaac-chittilappily-ada <isaac.chittilappily@ada.ac.uk>
2025-10-07 10:40:35 +0000

    Activity 2

--- 2_shopping_calculator.py
+++ 2_shopping_calculator.py
@@ -1,10 +1,20 @@
-def calculate_total(price, tax_rate=0.20, discount=0):
+def calculate_total(price, tax_rate=0.20, discount=0, tip=0.12):
     subtotal = price - discount
     tax = subtotal * tax_rate
     total = subtotal + tax
+    total += tip
     return total
+
+
+def analyse_sales_data(sales: list):
+    total = sum(sales)
+    avg = total/len(sales)
+    best_month = max(sales)
+    worst_month = min(sales)
+
+    return total, avg, best_month, worst_month
 
 # Test cases
 print(f"£{calculate_total(100):.2f}")
-print(f"£{calculate_total(100, 0.1):.2f}")
+print(f"£{calculate_total(100, tax_rate=0.1):.2f}")
 print(f"£{calculate_total(100, 0.08, 10):.2f}")
